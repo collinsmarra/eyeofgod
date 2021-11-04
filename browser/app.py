ee3fe68addcd8586e3ffcb6c80c6d4c939e65835
Collins Kiplagat <collinsk498@gmail.com>
2021-11-04 10:52:23 +0300

    Modified app.py

--- browser/app.py
+++ browser/app.py
@@ -3,15 +3,18 @@ import cv2
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture('uneskie.mkv')  # replace with address for camera stream.
+#: camera = cv2.VideoCapture('uneskie.mkv')  # replace with address for camera stream.
 
 def gen_frames():
-	while True:
-		success, frame = camera.read()
-		ret, buffer = cv2.imencode('.jpg', frame)
-		frame = buffer.tobytes()
-		yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    camera = cv2.VideoCapture(0)
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/video_feed')
